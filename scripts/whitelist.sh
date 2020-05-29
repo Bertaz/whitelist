c4878633aea719d560a8b525a79556c6260fbde1
Bertaz <44445647+Bertaz@users.noreply.github.com>
2020-05-29 14:01:19 +0200

    Makes script local
    Removed upstream download and old pihole usage

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -5,7 +5,8 @@
 # Created by Anudeep (Slight change by cminion)
 #================================================================================
 TICK="[\e[32m ✔ \e[0m]"
-PIHOLE_LOCATION="/etc/pihole"
+# PIHOLE_LOCATION="/etc/pihole"
+TEMP_LOCATION="/opt/whitelist"
 GRAVITY_UPDATE_COMMAND="pihole -w -q"
 
 echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
@@ -19,29 +20,33 @@ if [ "$(id -u)" != "0" ] ; then
 	exit 2
 fi
 
-curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | tee "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+# curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | tee "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+cp -f "${TEMP_LOCATION}"/domains/whitelist.txt "${TEMP_LOCATION}"/whitelist.txt.old
 echo -e " ${TICK} \e[32m Adding domains to whitelist... \e[0m"
 sleep 0.1
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && sort -u "${PIHOLE_LOCATION}"/whitelist.txt.old > "${PIHOLE_LOCATION}"/whitelist.txt
-rm "${PIHOLE_LOCATION}"/whitelist.txt.old
+# mv "${TEMP_LOCATION}"/whitelist.txt "${TEMP_LOCATION}"/whitelist.txt.old && 
+sort -u "${TEMP_LOCATION}"/whitelist.txt.old > "${TEMP_LOCATION}"/whitelist.txt
+rm "${TEMP_LOCATION}"/whitelist.txt.old
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-${GRAVITY_UPDATE_COMMAND} $(xargs < "${PIHOLE_LOCATION}"/whitelist.txt) --comment "AnudeepND/Whitelist" #> /dev/null
-rm "${PIHOLE_LOCATION}"/whitelist.txt
+${GRAVITY_UPDATE_COMMAND} $(xargs < "${TEMP_LOCATION}"/whitelist.txt) --comment "AnudeepND/Whitelist" #> /dev/null
+rm "${TEMP_LOCATION}"/whitelist.txt
 
 sleep 0.2
 
-curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt | tee "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+# curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt | tee "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+cp -f "${TEMP_LOCATION}"/domains/optional-list.txt "${TEMP_LOCATION}"/whitelist.txt.old
 echo -e " ${TICK} \e[32m Adding optional domains to whitelist... \e[0m"
 sleep 0.1
 echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && sort -u "${PIHOLE_LOCATION}"/whitelist.txt.old | sed -n '/^\s*$/d; /^#/!p' > "${PIHOLE_LOCATION}"/whitelist.txt
-rm "${PIHOLE_LOCATION}"/whitelist.txt.old
+# mv "${TEMP_LOCATION}"/whitelist.txt "${TEMP_LOCATION}"/whitelist.txt.old && 
+sort -u "${TEMP_LOCATION}"/whitelist.txt.old | sed -n '/^\s*$/d; /^#/!p' > "${TEMP_LOCATION}"/whitelist.txt
+rm "${TEMP_LOCATION}"/whitelist.txt.old
 
 echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-${GRAVITY_UPDATE_COMMAND} $(xargs < "${PIHOLE_LOCATION}"/whitelist.txt) --comment "AnudeepND/Optional" #> /dev/null
-rm "${PIHOLE_LOCATION}"/whitelist.txt
+${GRAVITY_UPDATE_COMMAND} $(xargs < "${TEMP_LOCATION}"/whitelist.txt) --comment "AnudeepND/Optional" #> /dev/null
+rm "${TEMP_LOCATION}"/whitelist.txt
 
 echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
